70ab8bc503fd5079a75def3f56b8358e2a2a8fb6
ik012982i9 <ik012982i9@220-13-HZ307520.H012982.NET>
2020-02-20 11:24:38 +0100

    Hola Amaia

---
+++ Subir GIT/src/gggg/holamundo.java
@@ -0,0 +1,15 @@
+package gggg;
+
+/*
+ * Ejemplo HolaMundo 
+ * Imprime el mensaje "Hola, Mundo!" 
+*/
+   
+class HolaMundo {
+   public static void main( String[] args ){
+      
+       System.out.println("Hola, Mundo desde Java!");
+       
+   }
+
+}
